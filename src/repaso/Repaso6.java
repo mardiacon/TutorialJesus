6283a9356d5d282c5c36e05080c2bcd3d95370e1
mardiacon <marta_diaz30@hotmail.com>
2022-01-04 17:59:01 +0100

    Poo

--- src/repaso/Repaso6.java
+++ src/repaso/Repaso6.java
@@ -1,7 +1,5 @@
 package repaso;
 
-import java.util.Scanner;
-
 public class Repaso6 {
 
 	/*
@@ -14,22 +12,5 @@ public class Repaso6 {
 	
 	public static void main(String[] args) {
 		
-		System.out.print("Introduce una hora: ");
-		Scanner teclado = new Scanner(System.in);
-		int hora = teclado.nextInt();
-		
-		switch(hora) {
-		case 6: case 7: case 8: case 9: case 10: case 11: 
-			System.out.println("Es por la mañana.");
-			break;
-		case 12: case 13: case 14: case 15:
-			System.out.println("Es medio día.");
-		case 16: case 17: case 18: case 19: case 20:
-			System.out.println("Es por la tarde.");
-		case 21: case 22: case 23: case 24:
-			System.out.println("Es por la noche");
-		case 1: case 2: case 3: case 4: case 5:
-			System.out.println("Es de madrugada.");
-		}
 	}
 }
